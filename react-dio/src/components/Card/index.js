546c0edd9518ae39690d3bb78dee35e3a6d84334
francienevaz <fran.vfraga@gmail.com>
2022-01-21 18:15:03 -0300

    updated

---
+++ react-dio/src/components/Card/index.js
@@ -0,0 +1,14 @@
+const Card = () => {
+    return (
+        <div className="card">
+            <div class="card-header">
+                Featured
+            </div>
+            <div className="card-body">
+                <p>Meu texto no Card</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- react-dio/src/components/Item/index.js
+++ react-dio/src/components/Item/index.js
@@ -1,6 +1,6 @@
 const Item = ({children}) => {
     return (
-        <a href="/" class="list-group-item list-group-item-action list-group-item-danger">{children}</a>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-danger">{children}</a>
     )
 }
 
