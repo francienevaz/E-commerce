28e8d58f50ce1c20b6843aaa562b8226464c7523
francienevaz <fran.vfraga@gmail.com>
2022-01-21 19:57:11 -0300

    updated

--- react-dio/src/components/Card/index.js
+++ react-dio/src/components/Card/index.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 const Card = () => {
+    const [valor, setValor] = useState(0)
+
+    function Add () {
+        setValor(valor + 1)
+    }
+    function Remove () {
+        setValor(valor - 1)
+    }
+    
+
     return (
         <div className="card">
-            <div class="card-header">
-                Featured
+            <div className="card-header">
+                Meu primeiro Card
             </div>
             <div className="card-body">
-                <p>Meu texto no Card</p>
+            <button 
+                type="button" 
+                className="btn btn-success"
+                onClick={Add}>Add
+            </button>
+
+            <button 
+                type="button" 
+                className="btn btn-success"
+                onClick={Remove}>Remove
+            </button>
+            <p>{valor}</p>
+            
             </div>
         </div>
     )
