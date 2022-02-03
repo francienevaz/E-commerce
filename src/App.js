e722d65dd43214f6cf7e39c3ee4fd0ab49aeb74c
francienevaz <fran.vfraga@gmail.com>
2022-02-03 17:32:07 -0300

    updated - implementing new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import Item from "./components/Item";
-import Card from "./components/Card";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './components/store';
+import Routes from './routes';
+import { Container } from '@material-ui/core/';
+import Header from './components/Header';
 
 const App = () => {
+  
+  const localCart = JSON.parse(localStorage.getItem('dioshopping: cart'))
+  
+  if(localCart !== null) {
+    store.dispatch({type: 'CHANGE_CART', localCart})
+  }
+  
   return(
-    <>
-    <h1>Minha primeira aplicação com React</h1>
-    <ul>
-      <Item>
-        Item 1
-      </Item>
-      <Item>
-        Item 2
-      </Item>
-      <Item>
-        Item 3
-      </Item>      
-    </ul>
-    <Card/>
-    </>
-    
+    <Provider store={store}>
+      <Container maxWidth="xl">
+        <Router>
+          <Header />
+          <Routes />
+        </Router>
+      </Container> 
+    </Provider>
   )
 }
 
